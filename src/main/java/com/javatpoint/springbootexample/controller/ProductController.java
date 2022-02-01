775f689d65c689abe5e8e8a1fd2cc363206a465d
SATHISH <sathish.nagappan@fisglobal.com>
2022-02-01 09:37:34 +0530

    'chnagesAdded'

---
+++ src/main/java/com/javatpoint/springbootexample/controller/ProductController.java
@@ -0,0 +1,14 @@
+package com.javatpoint.springbootexample.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+	
+	@GetMapping("getProdcts")
+	public String getAllProsucts () {
+		return "sample data";
+	}
+	
+}
