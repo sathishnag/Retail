88664c69ff06c4ac86df9751d38d17e40eceb012
sathishnag <43461899+sathishnag@users.noreply.github.com>
2022-02-02 22:50:22 +0530

    Update ProductController.java

--- src/main/java/com/javatpoint/springbootexample/controller/ProductController.java
+++ src/main/java/com/javatpoint/springbootexample/controller/ProductController.java
@@ -3,13 +3,14 @@ package com.javatpoint.springbootexample.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 
 @RestController
 public class ProductController {
 	
 	@GetMapping("getProdcts")
 	public ResponseEntity<String> getAllProsucts () {
-		return ResponseEntity.ok("welcome");
+		return new ResponseEntity<>("test"), HttpStatus.OK);
 	}
 	
 }
